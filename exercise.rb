8ada97c4becc1e4637bddacfe5e887904baf8b81
msynko <marina.synko@gmail.com>
2018-12-13 11:38:08 -0500

    second commit

--- exercise.rb
+++ exercise.rb
@@ -1,15 +1,22 @@
 #Write a method that accepts a number as an argument and returns a string containing that number along with its "ordinal indicator". E.g. passing in 1 would return "1st", 2 would return "2nd", 3 would return "3rd", 4 would return "4th", etc.
-
 def ordinal(n)
-  if n == 1
-    return "#{n}st"
-  elsif n == 2
-    return "#{n}nd"
-  elsif n == 3
-    return "#{n}rd"
-  else
-    return "#{n}th"
-  end
+  ending = case n % 100
+           when 11, 12, 13 then 'th'
+           else
+             case n % 10
+             when 1 then 'st'
+             when 2 then 'nd'
+             when 3 then 'rd'
+             else 'th'
+             end
+           end
+
+  "This is #{n}#{ending} item"
 end
 
 puts ordinal(1) # => "This is 1st item"
+puts ordinal(10) # => "This is 10th item"
+puts ordinal(12) # => "This is 12th item"
+puts ordinal(15) # => "This is 15th item"
+puts ordinal(112) # => "This is 112th item"
+puts ordinal(123) # => "This is 123rd item"
